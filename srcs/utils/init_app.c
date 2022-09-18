efa7dc91d7253105c44aefa15c4a3a8c4b0e4576
Altagracia Cierra <alcierra@student.21-school.ru>
2022-09-18 19:01:11 +0300

    init_app error fixed

--- srcs/utils/init_app.c
+++ srcs/utils/init_app.c
@@ -2,6 +2,7 @@
 
 int	init_app(t_app *app, int argc, char **argv, char **env)
 {
+	(void) argc; (void) argv;
 	app->env = init_env(env);
 	if (!app->env)
 		exit_alert("cannot allocate memory\n", ERROR_ENV_INIT);
